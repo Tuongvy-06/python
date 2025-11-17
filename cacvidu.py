006c2ad5d8fc52a2fc51e84eb2a9e5ea05dc87e4
Lam Tuong Vy <Vyytuong32@gmail.com>
2025-11-17 16:05:56 +0700

    first commit

---
+++ cacvidu.py
@@ -0,0 +1,20 @@
+#vidu1
+i=2
+while i<=10:
+    print(f"{i}", end=" ")
+    i+=2
+#vidu2
+i=3
+while i<=18:
+    print(f"{i}", end=" ")
+    i+=3
+#vidu3
+i=5
+while i<=100:
+    print(f"{i}", end=" ")
+    i+=5
+#vidu4
+i=1
+while i<=99:
+    print(f"{i}", end="+")
+    i+=1
